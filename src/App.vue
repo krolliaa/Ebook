f411a32983ae79f7d8c344b9c6d9759736b9faad
krolliaa <krolliaa@163.com>
2021-04-22 21:54:12 +0800

    第三章 项目准备

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <span class="text1">ABCDEFG</span><br/>
+    <span class="text2">HIJKLMN</span>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import {mapGetters} from 'vuex'
 
-#nav {
-  padding: 30px;
-}
+  const getters = {
+    a: () => 1,
+    b: () => 2
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  function fn(keys) {
+    let data = {};
+    keys.forEach(key => {
+      if (getters.hasOwnProperty(key)) {
+        data[key] = getters[key];
+      }
+    });
+    return data;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  export default {
+    computed: {
+      ...mapGetters(['getBookTest']),
+      ...fn(['a', 'b', 'c'])
+    },
+    mounted() {
+      this.$store.dispatch("setTest", 10);
+      // console.log(this.$store.state.book.test);
+      // console.log(this.getBookTest);
+      console.log(this.a, this.b);
+    }
+  };
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html');
+    let fontSize = window.innerWidth / 10;
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px';
+  });
+</script>
+
+<style lang="scss" scoped>
+  @import "./assets/styles/global.scss";
+
+  .text1 {
+    font-family: "Days One";
+    font-size: px2rem(20);
+    color: chocolate;
+  }
+
+  .text2 {
+    font-family: "Montserrat";
+    font-size: px2rem(20);
+    color: orangered;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {book} from './modules/book.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    "book": book
+  },
+  getters: {
+    getBookTest: state => {
+      return state.book.test;
+    }
   }
 })
